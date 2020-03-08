f216a7d2a91970b149df03517c30899a05f9ec24
Christian Berendt <berendt@betacloud-solutions.de>
2020-03-08 16:19:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# image-syncer
+
+[![Build Status](https://travis-ci.org/osism/image-syncer.svg?branch=master)](https://travis-ci.org/osism/image-syncer)
+
+Simple Python script to copy our latest images regularly from our
+development registry ([Quay.io](https://quay.io/osism/)) to
+[Docker Hub](https://hub.docker.com/r/osism).
---
+++ src/main.py
@@ -0,0 +1,55 @@
+import logging
+
+import docker
+from tabulate import tabulate
+import yaml
+
+logging.basicConfig(
+    format='%(asctime)s - %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+client = docker.APIClient(base_url='unix:///var/run/docker.sock')
+
+with open("etc/images.yml", "rb") as fp:
+    images = yaml.load(fp, Loader=yaml.SafeLoader)
+
+result = []
+
+for image in images:
+    logging.info("processing image %s" % image)
+
+    source = images[image]['source']
+    target = images[image]['target']
+
+    for tag in images[image]['tags']:
+        logging.info("processing tag %s" % tag)
+
+        logging.info("pulling - %s:%s" % (source, tag))
+        client.pull(source, tag)
+
+        docker_image = client.inspect_image("%s:%s" % (source, tag))
+        result.append([source, tag, docker_image["Id"],
+                       docker_image["Created"]])
+
+        logging.info("tagging - %s:%s" % (target, tag))
+        client.tag("%s:%s" % (source, tag), target, tag)
+
+        logging.info("pushing - %s:%s" % (target, tag))
+        client.push(target, tag)
+
+for image in images:
+
+    source = images[image]['source']
+    target = images[image]['target']
+
+    for tag in images[image]['tags']:
+
+        logging.info("removing - %s:%s" % (source, tag))
+        client.remove_image("%s:%s" % (source, tag))
+
+        logging.info("removing - %s:%s" % (target, tag))
+        client.remove_image("%s:%s" % (target, tag))
+
+print(tabulate(result, headers=["Image", "Tag", "Hash", "Created"]))
